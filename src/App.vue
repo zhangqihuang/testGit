4af88956bb78ea3189b0fd2206b13bfc44f5c13e
zhangqihuang <995418122@qq.com>
2018-08-25 16:49:35 +0800

    b add box component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Box/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +8,15 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+import Box from './components/Box.vue'
+export default {
+  components:{
+    Box
+  }
+}
+</script>
 
 <style lang="less">
 #app {
